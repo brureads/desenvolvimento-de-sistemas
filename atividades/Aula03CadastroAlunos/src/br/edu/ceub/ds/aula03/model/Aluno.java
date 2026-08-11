9aba4eb088063684abb72a9032f956e60bb11b64
Bruna Martins Moura <brumartins1304@gmail.com>
2026-08-11 10:29:52 -0300

    Aula 03

---
+++ atividades/Aula03CadastroAlunos/src/br/edu/ceub/ds/aula03/model/Aluno.java
@@ -0,0 +1,21 @@
+package br.edu.ceub.ds.aula03.model;
+
+public class Aluno {
+    private String nome;
+    private int semestre;
+    private String curso;
+
+    public Aluno(String nome, int semestre, String curso) {
+        this.nome = nome;
+        this.semestre = semestre;
+        this.curso = curso;
+    }
+
+    public String getNome() { return nome; }
+    public int getSemestre() { return semestre; }
+    public String getCurso() { return curso; }
+
+    public String gerarResumo() {
+        return nome + " - " + curso + " - " + semestre + "º semestre";
+    }
+}
