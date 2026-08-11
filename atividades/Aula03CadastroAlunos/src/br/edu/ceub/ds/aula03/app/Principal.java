6f01010fd062b67320ec75baebb4ede032aedd7c
Bruna Martins Moura <brumartins1304@gmail.com>
2026-08-11 10:30:47 -0300

    Atividade 03

---
+++ atividades/Aula03CadastroAlunos/src/br/edu/ceub/ds/aula03/app/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.ceub.ds.aula03.app;
+
+import br.edu.ceub.ds.aula03.view.TelaCadastroAluno;
+
+public class Principal {
+    public static void main(String[] args) {
+        TelaCadastroAluno tela = new TelaCadastroAluno();
+        tela.setVisible(true);
+    }
+}
+
